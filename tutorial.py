ac9798d5454cdab5929da2dc9f7876858e749701
NickZach2036 <nikolay.n.zahariev.2020@elsys-bg.org>
2022-04-03 14:55:16 +0300

    Tutorial code changes and explanations

--- tutorial.py
+++ tutorial.py
@@ -1,13 +1,13 @@
 import pygame
 import os
 
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = 900, 500 #Създават се променливи за ширина и височина на прозореца;
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) #Променливите се вкарват във вградената функция. Тук на тяхно място могат направо да се вкарат числени стойности, но е по-лесно с променливи, тъй като ако решим да променим нещо, трябва да променим само на едно място, не навсякъде;
 #pygame.display.set.caption("First Game!")
 
-WHITE = (255, 255, 255)
+WHITE = (255, 255, 255) #Прави се променлива, която да задава цветовете;
 
-FPS = 60
+FPS = 60 #Задава се скорост, с която да върви играта;
 
 #YELLOW_SPACESHIP_IMAGE = pygame.image.load(
 #    os.path.join('Assets', 'spaceship_yellow.png'))
@@ -16,19 +16,19 @@ FPS = 60
 
 
 def draw_window():
-    WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
-    pygame.display.update()
+    WIN.fill(WHITE) #Задава се цвета на фона да е като цвета на променливата, отново като при ред 5;
+#    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    pygame.display.update() #Ъпдейтва дисплея след всяка промяна, иначе нищо няма да се покаже;
 
 
 def main():
     clock = pygame.time.Clock()
-    run = True
+    run = True #Задаваме прозорецът да стои постоянно отворен;
     while run:
-        clock.tick(FPS)
+        clock.tick(FPS) #Задаваме на всички устройства играта да върви с еднаква скорост;
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                run = False #За последните 3 реда: Прозорецът да се затваря само, ако се натисне X;
 
         draw_window()
 
