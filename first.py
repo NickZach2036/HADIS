7b8ee81315d3a7f96f85ce7201f63a892022fe6f
Hlebaria <mitigr@abv.bg>
2022-04-03 17:45:36 +0300

    Added first images and MouseCursor

--- first.py
+++ first.py
@@ -1,1 +1,30 @@
 #this is the first work file
+import pygame
+import os
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode([1000, 500])
+pygame.display.set_caption("HADIS")
+
+BACKGROUND = pygame.image.load(os.path.join('images', 'BIGGERSpace.png'))
+IMAGE = pygame.image.load(os.path.join('images', 'lazerPointTransparent.png'))
+WIN = pygame.display.set_mode((1000, 500))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    WIN.fill((0,0,0))
+    WIN.blit(BACKGROUND, (0,0))
+
+    MX, MY = pygame.mouse.get_pos()
+    WIN.blit(IMAGE, (MX-3,MY-3))
+    pygame.mouse.set_visible(False)
+
+    pygame.display.flip()
+
+pygame.quit()
