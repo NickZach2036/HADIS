b5fec8a694c3cb369d7e1682131cd31c79b144f1
Hlebaria <mitigr@abv.bg>
2022-04-04 19:54:57 +0300

    Added a hook mechanic and tested out mask colision in a seperate file

--- MaskColisionTest.py
+++ MaskColisionTest.py
@@ -1,4 +1,4 @@
-#this is the first work file
+
 import pygame
 import os
 from pygame.locals import *
@@ -13,25 +13,18 @@ pygame.display.set_caption("HADIS")
 
 DuckX = 100
 DuckY = 300
-angle = 0
-hooked = False
 
 BACKGROUND = pygame.image.load(os.path.join('images', 'BIGGERSpace.png'))
 CURSOR = pygame.image.load(os.path.join('images', 'lazerPointTransparent.png'))
 DUCK = pygame.image.load(os.path.join('images','yellowDuckTransparent.png'))
-CAUGHT = pygame.image.load(os.path.join('images', 'yellow_coughtTransparent.png'))
-CAUGHT = pygame.transform.scale(CAUGHT, (140, 140))
 DUCK = pygame.transform.scale(DUCK, (100, 100))
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def mouse_on(MX,MY,x,y):
-    ok = False
-    if(MX>=x+60):
-        if(MX<=x+75):
-            if(MY>=y):
-                if(MY<=y+15):
-                    ok = True
-    return ok
+DUCK_MASK = pygame.mask.from_surface(DUCK)
+DUCK_RECT = DUCK.get_rect()
+
+CURSOR_MASK = pygame.mask.from_surface(CURSOR)
+CURSOR_RECT = CURSOR.get_rect()
 
 running = True
 while running:
@@ -46,30 +39,16 @@ while running:
     WIN.blit(CURSOR, (MX-3,MY-3))
     pygame.mouse.set_visible(False)
 
-    #if (DuckX>(WIDTH+100)): DuckX = -100
+    WIN.blit(DUCK,(DuckX,DuckY))
 
-   # DUCK_COPY = pygame.transform.rotate(BIG_DUCK, angle)
-   # WIN.blit (DUCK_COPY, (DuckX - int(DUCK_COPY.get_width() / 2) , DuckY - int(DUCK_COPY.get_height() / 2)))
-    DUCK = pygame.transform.rotate(DUCK, angle)
-    #WIN.blit (CURSOR, (DuckX+70,DuckY+10))
+    offset = (DuckX-MX, DuckY-MY)
+    result = CURSOR_MASK.overlap(DUCK_MASK, offset)
 
-    if(mouse_on(MX,MY,DuckX,DuckY)):
-        hooked = True
-
-    if(hooked):
-        DuckX = MX-87
-        DuckY = MY-18
-        WIN.blit(CAUGHT,(DuckX,DuckY))
-    else:
+    if result:
+        DuckX, DuckY = 0,300
         WIN.blit(DUCK,(DuckX,DuckY))
-
-
-
-   # angle = angle + 3
-
-    DuckX = DuckX + 3
-    DuckY = DuckY
-
+        
+    DuckX = DuckX+1
     pygame.display.flip()
 
 pygame.quit()
