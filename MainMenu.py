c6eb7b8da32565b66f45ab0ad12eec7a243eaec8
NickZach2036 <nikolay.n.zahariev.2020@elsys-bg.org>
2022-04-04 16:56:52 +0300

    Implementation

--- MainMenu.py
+++ MainMenu.py
@@ -6,7 +6,47 @@ pygame.init()
 surface = pygame.display.set_mode((1366, 708))
 
 def start_the_game():
-    # Do the job here !
+    WIGHT, HEIGHT = 1250, 750
+
+    pygame.init()
+
+    screen = pygame.display.set_mode([WIGHT, HEIGHT])
+    LOGO = pygame.image.load(os.path.join('Game pixel art', 'logo.png'))
+    pygame.display.set_icon(LOGO)
+    pygame.display.set_caption("HADIS")
+
+    DuckX = 100
+    DuckY = 100
+
+    BACKGROUND = pygame.image.load(os.path.join('Game pixel art', 'BIGGERSpace.png'))
+    IMAGE = pygame.image.load(os.path.join('Game pixel art', 'lazerPointTransparent.png'))
+    DUCK = pygame.image.load(os.path.join('Game pixel art', 'yellowDuckTransparent.png'))
+    BIG_DUCK = pygame.transform.scale(DUCK, (100, 100))
+    WIN = pygame.display.set_mode((WIGHT, HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        WIN.fill((0, 0, 0))
+        WIN.blit(BACKGROUND, (0, 0))
+
+        MX, MY = pygame.mouse.get_pos()
+        WIN.blit(IMAGE, (MX - 3, MY - 3))
+        pygame.mouse.set_visible(False)
+
+        WIN.blit(BIG_DUCK, (DuckX, DuckY))
+        BIG_DUCK = pygame.transform.rotate(BIG_DUCK, -1)
+        # if (DuckX>WIGHT): DuckX = -100
+
+        # DuckX = DuckX + 2
+        # DuckY = DuckY
+
+        pygame.display.flip()
+
+    pygame.quit()
     pass
 
 mytheme = pygame_menu.themes.THEME_DARK.copy()
