a67399dc184c0b2ecaeaa05e5021867d07ca41b3
NickZach2036 <nikolay.n.zahariev.2020@elsys-bg.org>
2022-04-05 15:23:40 +0300

    Presentation files & stuff

--- MainMenu.py
+++ MainMenu.py
@@ -49,6 +49,20 @@ def start_the_game():
     pygame.quit()
     pass
 
+music = True
+
+def settings():
+    if
+
+    def mouse_on(MX, MY, x, y):
+        ok = False
+        if (MX >= x + 60):
+            if (MX <= x + 75):
+                if (MY >= y):
+                    if (MY <= y + 15):
+                        ok = True
+        return ok
+
 mytheme = pygame_menu.themes.THEME_DARK.copy()
 BACKGROUND = pygame.image.load(os.path.join('Game pixel art', 'BIGGERSpace.png'))
 myimage = pygame_menu.baseimage.BaseImage(image_path=os.path.join('Game pixel art', 'BIGGERSpace.png'))
@@ -61,7 +75,7 @@ pygame.display.set_icon(hadislogo)
 pygame.display.set_caption("HADIS")
 
 menu.add.button('Play', start_the_game)
-menu.add.button('Settings')
+menu.add.button('Settings', settings)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
 menu.mainloop(surface)
